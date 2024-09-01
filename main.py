b96d083aa66fbb36b992b5279ad97aa0f8eaf6bb
HONG,HAO-SYUAN <pg785632541@gmail.com>
2024-09-01 17:43:23 +0800

    update formatter

--- main.py
+++ main.py
@@ -4,28 +4,29 @@ import asone
 import torch
 import os
 
+
 def main(args):
     filter_classes = args.filter_classes
 
     if filter_classes:
-        filter_classes = ['person']
+        filter_classes = ["person"]
 
     if args.use_cuda and torch.cuda.is_available():
         args.use_cuda = True
     else:
         args.use_cuda = False
-     
-    if sys.platform.startswith('darwin'):
-        detector = asone.YOLOV8N_MLMODEL 
+
+    if sys.platform.startswith("darwin"):
+        detector = asone.YOLOV8N_MLMODEL
     else:
         detector = asone.YOLOV8N_PYTORCH
-    
+
     detect = asone.ASOne(
         tracker=asone.NORFAIR,
         detector=detector,
         weights=args.weights,
         # sam_weights=args.sam_weights,
-        use_cuda=args.use_cuda
+        use_cuda=args.use_cuda,
     )
 
     track = detect.video_tracker(
@@ -36,9 +37,9 @@ def main(args):
         display=args.display,
         draw_trails=args.draw_trails,
         filter_classes=filter_classes,
-        class_names=None
+        class_names=None,
     )
-    
+
     # for model_output in track:
     #     bbox_xyxy = model_output.dets.bbox
     #     ids = model_output.dets.ids
@@ -74,26 +75,55 @@ def main(args):
             output_line = f"{frame_num}, {obj_id}, {x1}, {y1}, {width}, {height}, -1, -1, -1, -1\n"
             output_file.write(output_line)
     output_file.close()
-        
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('video_path', help='Path to input video')
-    parser.add_argument('--cpu', default=True, action='store_false', dest='use_cuda',
-                        help='run on cpu if not provided the program will run on gpu.')
-    parser.add_argument('--no_save', default=True, action='store_false',
-                        dest='save_result', help='whether or not save results')
-    parser.add_argument('--no_display', default=True, action='store_false',
-                        dest='display', help='whether or not display results on screen')
-    parser.add_argument('--output_dir', default='data/results',  help='Path to output directory')
-    parser.add_argument('--draw_trails', action='store_true', default=False,
-                        help='if provided object motion trails will be drawn.')
-    parser.add_argument('--filter_classes', default=None, help='Filter class name')
-    parser.add_argument('-w', '--weights', default=None, help='Path of trained weights')
-    parser.add_argument('--sam_weights', default=None, help='Path of sam weights')
-    parser.add_argument('-ct', '--conf_thres', default=0.25, type=float, help='confidence score threshold')
-    parser.add_argument('-it', '--iou_thres', default=0.45, type=float, help='iou score threshold')
+    parser.add_argument("video_path", help="Path to input video")
+    parser.add_argument(
+        "--cpu",
+        default=True,
+        action="store_false",
+        dest="use_cuda",
+        help="run on cpu if not provided the program will run on gpu.",
+    )
+    parser.add_argument(
+        "--no_save",
+        default=True,
+        action="store_false",
+        dest="save_result",
+        help="whether or not save results",
+    )
+    parser.add_argument(
+        "--no_display",
+        default=True,
+        action="store_false",
+        dest="display",
+        help="whether or not display results on screen",
+    )
+    parser.add_argument(
+        "--output_dir", default="data/results", help="Path to output directory"
+    )
+    parser.add_argument(
+        "--draw_trails",
+        action="store_true",
+        default=False,
+        help="if provided object motion trails will be drawn.",
+    )
+    parser.add_argument("--filter_classes", default=None, help="Filter class name")
+    parser.add_argument("-w", "--weights", default=None, help="Path of trained weights")
+    parser.add_argument("--sam_weights", default=None, help="Path of sam weights")
+    parser.add_argument(
+        "-ct",
+        "--conf_thres",
+        default=0.25,
+        type=float,
+        help="confidence score threshold",
+    )
+    parser.add_argument(
+        "-it", "--iou_thres", default=0.45, type=float, help="iou score threshold"
+    )
 
     args = parser.parse_args()
 
